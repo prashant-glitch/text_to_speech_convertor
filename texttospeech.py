176cf168d1477dc0d3a17b4617d23134f16b3e49
prashant kumar <76524080+prashant-glitch@users.noreply.github.com>
2021-04-05 19:29:35 +0530

    Add files via upload

---
+++ texttospeech.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine=pyttsx3.init()
+engine.say('Hey this is prashant kumar. i am from nawada and right now i am in kolkata. I am the student of jadavpur university')
+engine.runAndWait()
